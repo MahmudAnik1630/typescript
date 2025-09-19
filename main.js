06fb1713a60c4c9f40902a6fb4c78f39fafc43b4
Md Mahmud Ul Hasan <mahmudulhasananik40@gmail.com>
2025-09-19 11:27:59 +0600

    typescript basic

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var message = 'Hello, TypeScript! welcome back';
+console.log(message);
+var x = 10;
+var y = 20;
+var sum;
+console.log(x + y);
---
+++ main.ts
@@ -0,0 +1,9 @@
+export {};
+let message = 'Hello, TypeScript! welcome back';
+console.log(message);
+
+let x = 10;
+const y = 20;
+let sum;
+
+console.log(x+y);
